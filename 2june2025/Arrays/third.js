d764258b7303cf65f17fc4f4da8527a9432937a5
Saif Alam <kaif0350@gmail.com>
2025-06-09 11:22:27 +0530

    solved today

--- 2june2025/Arrays/third.js
+++ 2june2025/Arrays/third.js
@@ -1,6 +1,6 @@
 // Why does map return a value and forEach does not?
 
 const result1 = [1, 2, 3].map((x) => x * 2);
-const result2 = [1, 2, 3].forEach((x) => x * 2);
+const result2 = [1, 2, 3].forEach((x) => x);
 console.log(result1); // ?
 console.log(result2); // ?
