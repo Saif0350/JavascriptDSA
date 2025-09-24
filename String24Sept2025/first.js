2426373824869462f8ea23ef310b61aed37f0881
Saif Alam <kaif0350@gmail.com>
2025-09-24 17:31:34 +0530

    learned string today

---
+++ String24Sept2025/first.js
@@ -0,0 +1,23 @@
+// Longest Substring Without Repeating Characters
+
+const str = "abcabcbb";
+
+function lengthOfLongestSubstring(s) {
+  let set = new Set();
+  let left = 0,
+    maxLen = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    while (set.has(s[right])) {
+      set.delete(s[left]);
+      left++;
+    }
+
+    set.add(s[right]);
+    maxLen = Math.max(maxLen, right - left + 1);
+  }
+
+  return maxLen;
+}
+
+console.log(lengthOfLongestSubstring(str));
